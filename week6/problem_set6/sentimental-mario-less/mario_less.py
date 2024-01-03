1cd0eae6a3c342653baea5f7a314a51e61ef5172
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2024-01-03 13:50:37 +0100

    mario_less

---
+++ week6/problem_set6/sentimental-mario-less/mario_less.py
@@ -0,0 +1,19 @@
+def main():
+    height = get_height()
+    for i in range(1, height + 1):
+        print(f"{(height - i) * " "}{'#' * i}")
+
+
+def get_height():
+    while True:
+        try:
+            height = int(input("Height: "))
+            if height > 0 and height < 9:
+                return height
+        except ValueError:
+            pass
+
+
+if __name__ == "__main__":
+    main()
+
